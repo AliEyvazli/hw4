3cb566fba92edac5271e8dd53ed10fe20d792d84
Ali Eyvazli <ali.eyvazli@kellogg.northwestern.edu>
2023-05-28 20:19:46 +0000

    solved logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   
   resources :places
   resources :posts
-  delete '/logout', to: 'sessions#destroy', as: :logout
+ 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   
+  get "logout", :to => "sessions#destroy", as: :logout
   get '/login', to: 'sessions#new', as: :login
   post '/login', to: 'sessions#create'
 end
